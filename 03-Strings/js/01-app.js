56fe0e7068f80ae4e025467f8bdec1fbf1e270b9
Rafael <rmartin65@gmail.com>
2024-05-07 18:17:47 +0100

    update string exercise

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,4 @@
+// los strings o cadenas de texto epesentan un texto que se puede reasigna a una variable
+// existen 3 formas de crealas
+const poducto = 'Monitor 20 pulgadas';
+const poducto2 = String('Monitor de 30 pulgadas');
