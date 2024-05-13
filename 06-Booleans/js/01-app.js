7cbe60f511e7829b63189d09bbf39a5d54180016
Rafael <rmartin65@gmail.com>
2024-05-13 17:00:43 +0100

    update booleans

--- 06-Booleans/js/01-app.js
+++ 06-Booleans/js/01-app.js
@@ -2,3 +2,13 @@
 //true o false
 const boolean1 = true;
 const boolean2 = false;
+
+console.log(boolean1);
+console.log(boolean2);
+
+console.log(typeof boolean2);
+
+//También un boolean se puede crear con la palabra new
+const boolean3 = new Boolean(true);
+console.log(boolean3);
+console.log(typeof boolean3);
---
+++ 06-Booleans/js/02-app.js
@@ -0,0 +1,9 @@
+//Comparar booleans es exactament igual que cualquie comparación de números o string
+
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(boolean1 === boolean2); //compara 2 variables
+console.log(boolean2 === true); //comparar si es verdadero
+// verdadero
+
