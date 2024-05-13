f314ff99c7e3022a17010f5511df51276058407b
Rafael <rmartin65@gmail.com>
2024-05-13 18:38:53 +0100

    update object

--- 07-Objetos/js/03-app.js
+++ 07-Objetos/js/03-app.js
@@ -1,1 +1,18 @@
-// 
+// como asigna vaiables a un objeto
+const producto = {
+    nombre: "Monitor de 30 pulgadas",
+    precio: 200,
+    disponible: true
+}
+
+//destructuring significa, saca de una estructura, pude ser complejo, no lo es tanto sobre todo cuando se practica.
+const {nombre} = producto;
+// si deseamos más variables
+const {precio} = producto;
+
+
+console.log(nombre);
+console.log(precio);
+
+// también se puede definir asi
+// const {nombre, precio} = producto;
