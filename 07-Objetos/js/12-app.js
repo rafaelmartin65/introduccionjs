7fae752992007682c557af087a15c62588a07761
Rafael <rmartin65@gmail.com>
2024-05-15 17:07:06 +0100

    update array

--- 07-Objetos/js/12-app.js
+++ 07-Objetos/js/12-app.js
@@ -1,1 +1,16 @@
-// 
+// Vamos a ver Object.Keys-Object.values y entries, estos son
+// más conocidos como iteradores de objetos
+const producto = {
+    nombre: 'Monitor 30 pulgadas',
+    precio: 200,
+    disponible: true,
+    
+}
+console.log(Object.keys(producto)); // devuelve un array que contiene las propiedades enumerables de un objeto llamado producto. Cada elemento del array corresponderá a una clave (propiedad) del objeto producto.
+
+console.log(Object.values(producto)); // devuelve un array que contiene los valores de las propiedades enumerables de un objeto llamado producto. Cada elemento del array corresponderá al valor de una propiedad del objeto producto.
+
+console.log(Object.entries(producto));// evuelve una matriz de matrices que representan cada par clave-valor del objeto producto. Cada sub-matriz tiene dos elementos: el primero es la clave (nombre de la propiedad) y el segundo es el valor correspondiente.
+
+// con esto tengo un buen conocimiento de objetos
+
---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,15 @@
+//Supongamos que creamos un carrito de compras desde la consola
+//más a delante lo haremos en una interfaz
+const meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio');
+console.log(meses);
+
+const numeros = [10,20,30,40,50];
+console.log(numeros);
+
+const deTodo = ["Hola", 10, true, "bastado", "si", null, Date(), {nombre: 'Rafael'}, [0,1,2,3,4,5] ];
+console.log(deTodo);
+// un array puede tener un aray dentro
+// encuentro más fácil utilizar la sistaxis de {}
+// es la que estaremos utilizando
+// recuerda que un  arreglo es una forma de arupar grandes
+// cantiddades de información en una sola variable
