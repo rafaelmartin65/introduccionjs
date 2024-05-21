7f18a8ed35cb17df2c26f1cc614531f0ea976d4e
Rafael <rmartin65@gmail.com>
2024-05-21 16:40:25 +0100

    update function

--- 08-Arrays/js/10-app.js
+++ 08-Arrays/js/10-app.js
@@ -1,10 +1,23 @@
 const carrito = [
-    {nombre: 'Monitor 20 pulgadas', precio:300},
-    {nombre: 'Televiion 50 pulgadas', precio: 200},
+    {nombre: 'Monitor 20 pulgadas', precio: 300},
+    {nombre: 'Television 50 pulgadas', precio: 200},
     {nombre: 'Tablet', precio: 300},
     {nombre: 'Audifonos', precio: 200},
     {nombre: 'Teclado', precio: 50},
     {nombre: 'Movil', precio: 500}
-    
-]
+];
+
+// Muy similar al foreach existe un array method llamado map, la diferencia es que te crea un nuevo array.
+
+const nuevoArray = carrito.map(function(producto){
+    return `Articulo: ${producto.nombre} precio: ${producto.precio}`;
+});
+
+carrito.forEach(item => {
+    console.log(`Artículo: ${item.nombre} Precio: ${item.precio}`);
+});
+
+console.log("Array 1 con método map:", nuevoArray);
+// ForEach no retorna un nuevo array, solo realiza una acción sobre cada elemento.
+console.log("Array 1 con forEach no genera un nuevo array, solo itera sobre los elementos.");
 
---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,21 @@
+/* Las funciones en cualquier lenguaje son una serie de procedimientos o 
+instrucciones que realizan una acción. Una de las ventajas de las funciones
+es que permiten tener un código más ordenado y fácil de mantener. */
+
+/* Otra de las ventajas de las funciones es que son reutilizables. Puedes tener una función 
+que valide un formulario y utilizarla en todos los formularios. También puedes tener 
+una función que envíe datos al servidor y reutilizarla miles de veces. */
+
+// Existen muchas formas de crear funciones en JavaScript.
+
+// Declaración de función: la declaración de la función utiliza la palabra 
+// reservada "function", seguida de un nombre y unos paréntesis. En estos
+// paréntesis se colocan los argumentos. Finalmente, el cuerpo de la 
+// función se define con unas llaves.
+
+
+function sumar(){
+    console.log(2 + 2);
+}
+
+sumar(); //llamada ala función sumar
