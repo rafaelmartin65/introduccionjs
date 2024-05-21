950a10f27410c3cae24585a8a9f9531fb7e4ada9
Rafael <rmartin65@gmail.com>
2024-05-21 16:06:47 +0100

    update

---
+++ 08-Arrays/js/10-app.js
@@ -0,0 +1,10 @@
+const carrito = [
+    {nombre: 'Monitor 20 pulgadas', precio:300},
+    {nombre: 'Televiion 50 pulgadas', precio: 200},
+    {nombre: 'Tablet', precio: 300},
+    {nombre: 'Audifonos', precio: 200},
+    {nombre: 'Teclado', precio: 50},
+    {nombre: 'Movil', precio: 500}
+    
+]
+
