af2d3e9f8a0f154137918cded47e92b2e066ac4c
Rafael <rmartin65@gmail.com>
2024-05-23 17:41:16 +0100

    estructuras

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,7 @@
+// de momento todo nuestro código se muestra de arriba hacia abajo salvo cuendo llamamos una función
+// Algunas veces van a desear que su código se ejecute en una condición
+
+//Cuando vamos a un cajero lo primero que haces es inserta tu tajeta, egundo introduces tu pin si el número está bien puedes continuar
+//si no te dirá que hay un error, si es válido despuès eliges la operación que deseas realizar, si quieres retirar dinero del cajero
+//va a evisar que tengas saldo.
+
