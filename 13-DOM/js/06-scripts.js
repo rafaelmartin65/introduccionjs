f2dfe37c4a5cb86d883f417bb04b6e4e2b94232f
Rafael <rmartin65@gmail.com>
2024-06-04 18:24:09 +0100

    update  DOM

--- 13-DOM/js/06-scripts.js
+++ 13-DOM/js/06-scripts.js
@@ -20,3 +20,16 @@ console.log("innerText -->", encabezado.innerText);
 const textoEncabezado = document.querySelector('.contenido-hero h1').textContent;
 console.log(textoEncabezado);
 
+//Ahora si puedes ver que estoy asignando nuevo texto a una variable 
+document.querySelector('.contenido-hero h1').textContent = 'Nuevo Heading';
+
+//Otra forma sería...
+const nuevoTexto = 'Nuevo Heading';
+document.querySelector('.contenido-hero h1').textContent = nuevoTexto;
+
+//También puedo cambiar una imagen
+const imagen = document.querySelector('.card img');
+console.log(imagen.src);
+
+//Cambiar la imagen
+imagen.src = 'img/hacer2.jpg';
