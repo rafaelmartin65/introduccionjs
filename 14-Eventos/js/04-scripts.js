12f04e8d624cd60db744f1927771bd7a63b671e2
Rafael <rmartin65@gmail.com>
2024-06-07 17:34:00 +0100

    update events

---
+++ 14-Eventos/js/04-scripts.js
@@ -0,0 +1,21 @@
+/** Vamos a ver los eventos para un formulaio, básicamente
+ * es uno pero es muy importante saberlo
+ */
+
+/** Cuando envias un formulario usualmente lo que este
+ * en el action, es la página que se abre
+ */
+
+const formulario = document.querySelector('.formulario');
+
+formulario.addEventListener('submit', e => {
+    e.preventDefault(); // Es para evitar que el navegador realice la acción por default
+    console.log('enviando...');
+    console.log('información que da \'e\'', e);
+    console.log('información que da \'e-target-method\'', e.target.method);
+    console.log('información que da \'e-target-action\'', e.target.action);
+})
+
+/** Realiza un script que simule el juego de piedra, papel o tijera 
+ *  
+*/
